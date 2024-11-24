eb733b79fb0275a82efb6bd7fae703a8ad61d519
aryaman0603 <aryamansharma3319@gmail.com>
2024-11-24 17:18:31 +0530

    push

--- Food-Del/backend/controllers/foodController.js
+++ Food-Del/backend/controllers/foodController.js
@@ -1,7 +1,8 @@
 import foodModel from "../models/foodModel.js";
 import fs from 'fs';
 
-// Add food item
+// Add Food Items
+
 const addFood = async (req, res) => {
     if (!req.file) {
         return res.status(400).json({ success: false, message: "No file uploaded." });
@@ -27,6 +28,33 @@ const addFood = async (req, res) => {
         fs.unlinkSync(`uploads/${image_filename}`);
         res.json({ success: false, message: "Error adding food." });
     }
-};
+}
 
-export { addFood };
+// List Food Items
+
+const listFood = async (req,res) => {
+    try {
+        const foods = await foodModel.find({})
+        res.json({ success: true, data: foods, message: "Food Listed"})
+    } catch (error) {
+        res.json({ success: false, message: "Error Listing Food"})
+    }
+}
+
+// Remove Food Items 
+
+const removeFood = async (req,res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`, ()=>{});
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({ success: true, message: "Food Removed"})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "Error Removing Food"})
+    }
+} 
+
+
+export { addFood, listFood, removeFood };
