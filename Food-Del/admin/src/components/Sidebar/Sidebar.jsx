92086994f0cbac0f80903f6f9629d0b5918b92a3
aryaman0603 <aryamansharma3319@gmail.com>
2024-11-29 20:56:18 +0530

    push

---
+++ Food-Del/admin/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Sidebar.css'
+import {assets} from '../../assets/assets'
+import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
+
+const Sidebar = () => {
+
+    const [color,setColor] = useState("");
+
+  return (
+    <div className='sidebar'>
+      <div className="sidebar-options">
+        <NavLink to='/add' onClick={()=>setColor("Add Items")} className={color==="Add Items"?"sidebar-active":"sidebar-option"}>
+          <img src={assets.add_icon} alt="" />
+          <p>Add Items</p>
+        </NavLink>
+        <NavLink to='/list' onClick={()=>setColor("List Items")} className={color==="List Items"?"sidebar-active":"sidebar-option"}>
+          <img src={assets.order_icon} alt="" />
+          <p>List Items</p>
+        </NavLink>
+        <NavLink to='order' onClick={()=>setColor("Orders")} className={color==="Orders"?"sidebar-active":"sidebar-option"}>
+          <img src={assets.order_icon} alt="" />
+          <p>Orders</p>
+        </NavLink>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
