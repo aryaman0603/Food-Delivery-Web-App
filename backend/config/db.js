05768d62407bfaa5875eb18e353631971fac8fe3
aryaman0603 <aryamansharma3319@gmail.com>
2025-09-25 20:00:45 +0530

    push

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://aryamansharma3319:448948411@cluster0.e5xnz.mongodb.net/Food-Del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(`mongodb+srv://${process.env.DbUserName}:${process.env.DbPassword}@cluster0.e5xnz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(()=>console.log("DB Connected"));
 }
