2c1cf8becacbf703deb55e581ad04cd9925816e2
aryaman0603 <aryamansharma3319@gmail.com>
2025-10-01 01:58:13 +0530

    push

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const placeOrder = async (req,res) => {
 
-    const frontend_url = process.env.FRONTEND_URL || "http://localhost:5173";
+    const frontend_url = process.env.FRONTEND_URL
+    // const frontend_url = process.env.FRONTEND_URL || "http://localhost:5173";
 
     try {
         const newOrder = new orderModel({
